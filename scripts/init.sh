eed509356c6123273191f379f1acda9c956de9d3
Eumel <Eumel@Ms-MacBook-Pro-2.local>
2013-12-05 19:51:47 +0100

    fixed install python gadata lib

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,4 @@
 curl -o gdata-2.0.18.tar.gz https://gdata-python-client.googlecode.com/files/gdata-2.0.18.tar.gz
-tar zxvf gdata-2.0.18.tar.gz
-python ./gdata-2.0.18/setup.py install
+tar zxf gdata-2.0.18.tar.gz
+cd /gdata-2.0.18/
+python setup.py install
