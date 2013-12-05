4a94b4c5a2ddd1ee68e18df9f924d0d5b71b60b2
Eumel <Eumel@Ms-MacBook-Pro-2.local>
2013-12-05 19:58:14 +0100

    fixed typo

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
 curl -o gdata-2.0.18.tar.gz https://gdata-python-client.googlecode.com/files/gdata-2.0.18.tar.gz
 tar zxf gdata-2.0.18.tar.gz
-cd /gdata-2.0.18/
+cd gdata-2.0.18
 python setup.py install
