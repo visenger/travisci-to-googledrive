f280cd046ef9bec86d27e7164f71ac6bb1ea7ea9
Larysa Visengeriyeva <visenger@gmail.com>
2013-12-23 20:38:04 +0100

    Update run_bench.sh

--- scripts/run_bench.sh
+++ scripts/run_bench.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env scala
-
-case class Person(name: String)
-
-object HelloWorld {
-  def main(args: Array[String]) {
-    val al = Person("Al")
-    println(al)
-  }
-}
-
-HelloWorld.main(args)
+whereis scala
