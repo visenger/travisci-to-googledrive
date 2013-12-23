fecf009b965182e15296db8b40941d8c4019c778
Larysa Visengeriyeva <visenger@gmail.com>
2013-12-23 17:18:21 +0100

    Update test.py

--- scripts/test.py
+++ scripts/test.py
@@ -8,15 +8,15 @@ import argparse
 parser = argparse.ArgumentParser()
 
 #{1} args.memory
-parser.add_argument("-m", "--memory", type=float,
+parser.add_argument("-m", "--memory", type=float, default=0.0
                     help="display a square of a given number")
 
 #{0} args.runtime
-parser.add_argument("-r", "--runtime", type=float,
+parser.add_argument("-r", "--runtime", type=float, default=0.0
                     help="display a square of a given number")                    
 args = parser.parse_args()
 
 
-url_str="https://docs.google.com/forms/d/1IFqNiEE5p67_tyy7tCevn8t_p8NDWcEQ3pgEd1Ggt40/formResponse?entry.111568791={0}&entry.559553889={1}".format(args.runtime, args.memory)
+url_str="https://docs.google.com/forms/d/1IFqNiEE5p67_tyy7tCevn8t_p8NDWcEQ3pgEd1Ggt40/formResponse?entry.111568791={runtime}&entry.559553889={memory}".format(runtime=args.runtime, memory=args.memory)
 urllib2.urlopen(url_str).read()
 #urllib2.urlopen("https://docs.google.com/forms/d/1IFqNiEE5p67_tyy7tCevn8t_p8NDWcEQ3pgEd1Ggt40/formResponse?entry.111568791=3.2&entry.559553889=5.8").read()
