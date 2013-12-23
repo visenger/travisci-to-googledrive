8a56d8905358a0c79f5985bf35f39761f45a9663
Larysa Visengeriyeva <visenger@gmail.com>
2013-12-23 18:37:18 +0100

    Update bench.sh

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Hello Travis ${TRAVIS_BUILD_ID}"
 #run benchmarks and pass bench results as arguments to the script -r runtime, -m memory
-python ./scripts/test.py -r 3.0 -m 5.5
+# declare RESULT=($(./myscript))  # (..) = array ---e.g benchmark returning numbers in proper(!) order.
+# echo "First line: ${RESULT[0]}"
+# echo "Second line: ${RESULT[1]}"
+# echo "N-th line: ${RESULT[N]}"
+
+python ./scripts/test.py -r 3.2 -m 5.1
